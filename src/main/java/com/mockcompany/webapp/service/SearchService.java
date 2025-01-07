27c10329a75d65d1fc89b32cee576913f1dd2b6f
Matthew1508 <mjschettino@outlook.com>
2025-01-07 19:37:13 +0000

    breaking it good and proper

--- src/main/java/com/mockcompany/webapp/service/SearchService.java
+++ src/main/java/com/mockcompany/webapp/service/SearchService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.*;
 
 @Service
 public class SearchService {
